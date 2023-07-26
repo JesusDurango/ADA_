cf616d859ebb5633abbcd1562de1e08b57dfcc38
Jesus Abrahan Durango <139592749+JesusDurango@users.noreply.github.com>
2023-07-26 00:12:55 -0500

    corrección de pequeños errores gramaticales

--- Variables_python.py
+++ Variables_python.py
@@ -7,10 +7,10 @@ print(f"Hola {nombre}, tu edad es {edad} y tu estatura es {estatura} cm, tomas a
 
 # Limite de los enteros y flotantes en python: 
 # Los enteros (int) tienen precisión ilimitada. 
-# Los Los flotantes (float) a diferencia de los enteros (int) tienen unos valores mínimo y máximos que pueden representar. 
+# Los flotantes (float) a diferencia de los enteros (int) tienen unos valores mínimo y máximos que pueden representar. 
 # La mínima precisión es 2.2250738585072014e-308 y la máxima 1.7976931348623157e+308 
 
-# Primeros numeros pares
+# Suma de los primeros numeros pares
 print("Ingrese el numero inicial")
 n = int(input())
 print("Ingrese el numero final")
